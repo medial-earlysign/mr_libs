34893a0be1a8778da417549a647fc9207ed340f7
alonyado <alonyado@users.noreply.github.com>
2026-01-15 13:18:33 +0000

    deploy: 9ad5959f71c15019fcbfee115fd42cc4e42dfeab

--- search/all_24.js
+++ search/all_24.js
@@ -4,7 +4,7 @@ var searchData=
   ['yarn_20appmaster_1',['DMLC YARN AppMaster',['../md_External_2xgboost_2dmlc-core_2tracker_2yarn_2README.html',1,'']]],
   ['yarn_5fsubmit_2',['yarn_submit',['../namespacedmlc__tracker_1_1yarn.html#aaada8f08e1d1ded3ccc0748e90a013a6',1,'dmlc_tracker::yarn']]],
   ['years_3',['years',['../md_External_2xgboost_2doc_2R-package_2discoverYourData.html#autotoc_md158',1,'Grouping per 10 years'],['../classMPTime.html#a751563e7291fd2a9558f02df7a39d1e4',1,'MPTime::Years'],['../classMedTime.html#ab06c5bc4f98b38c35ef8f1a5442e4aa8',1,'MedTime::Years']]],
-  ['years_3a_4',['Main contributers from recent years:',['../md_README.html#autotoc_md525',1,'']]],
+  ['years_3a_4',['Main contributers from recent years:',['../md_README.html#autotoc_md526',1,'']]],
   ['your_20c_20c_20applications_20using_20cmake_204323_204333_204453_5',['New feature: Embed XGBoost in your C/C++ applications using CMake (#4323, #4333, #4453)',['../md_External_2xgboost_2NEWS.html#autotoc_md416',1,'']]],
   ['your_20dataset_20with_20xgboost_6',['Understand your dataset with XGBoost',['../md_External_2xgboost_2doc_2R-package_2discoverYourData.html',1,'']]],
   ['your_20own_20plugin_7',['Write Your Own Plugin',['../md_External_2xgboost_2plugin_2README.html#autotoc_md475',1,'']]]
