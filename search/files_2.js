0a6a71586b1e376c32ef2d40e6c70f473382316c
alonyado <alonyado@users.noreply.github.com>
2026-05-18 14:31:05 +0000

    deploy: 12ddef957781dcea34bb66d3e75857af7903fafe

--- search/files_2.js
+++ search/files_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['c_2dapi_2ddemo_2ec_0',['c-api-demo.c',['../c-api-demo_8c.html',1,'']]],
-  ['c_5fapi_2eh_1',['c_api.h',['../xgboost_2include_2xgboost_2c__api_8h.html',1,'(Global Namespace)'],['../xgboost_2rabit_2include_2rabit_2c__api_8h.html',1,'(Global Namespace)']]],
+  ['c_5fapi_2eh_1',['c_api.h',['../xgboost_2rabit_2include_2rabit_2c__api_8h.html',1,'(Global Namespace)'],['../xgboost_2include_2xgboost_2c__api_8h.html',1,'(Global Namespace)']]],
   ['c_5fapi_5ferror_2ecc_2',['c_api_error.cc',['../c__api__error_8cc.html',1,'']]],
   ['c_5fapi_5ferror_2eh_3',['c_api_error.h',['../c__api__error_8h.html',1,'']]],
   ['cached_5finput_5fsplit_2eh_4',['cached_input_split.h',['../cached__input__split_8h.html',1,'']]],
@@ -12,7 +12,7 @@ var searchData=
   ['common_5frow_5fpartitioner_2eh_9',['common_row_partitioner.h',['../common__row__partitioner_8h.html',1,'']]],
   ['compressed_5fiterator_2eh_10',['compressed_iterator.h',['../compressed__iterator_8h.html',1,'']]],
   ['concurrency_2eh_11',['concurrency.h',['../concurrency_8h.html',1,'']]],
-  ['config_2eh_12',['config.h',['../xgboost_2dmlc-core_2include_2dmlc_2config_8h.html',1,'(Global Namespace)'],['../xgboost_2src_2common_2config_8h.html',1,'(Global Namespace)']]],
+  ['config_2eh_12',['config.h',['../xgboost_2src_2common_2config_8h.html',1,'(Global Namespace)'],['../xgboost_2dmlc-core_2include_2dmlc_2config_8h.html',1,'(Global Namespace)']]],
   ['context_2eh_13',['context.h',['../context_8h.html',1,'']]],
   ['csv_5fparser_2eh_14',['csv_parser.h',['../csv__parser_8h.html',1,'']]]
 ];
